83e59fbda62106050243bd36ee31c06bbcdcd432
alan-mak <alanmak95@gmail.com>
2022-03-21 16:55:12 -0400

    Works page not responsive

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const sass = require("node-sass-middleware");
 const port = process.env.PORT || 8080;
 const moment = require("moment");
+const dataObj = {moment: moment};
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -16,9 +17,14 @@ app.use("/styles", sass({
 }));
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index', {moment: moment});
+app.get('/works', (req, res) => {
+  res.render('works', dataObj);
 });
+
+app.get('/', (req, res) => {
+  res.render('index', dataObj);
+});
+
 
 app.listen(port, () => {
   console.log("Listening on: http://localhost:" + port);
